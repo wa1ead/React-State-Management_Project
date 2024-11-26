7e0de5d033831ab941217985982c9cfb5847b43d
wa1ead <waliddraa2@gmail.com>
2024-11-26 12:22:35 +0100

    importing logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,19 +6,12 @@ export default function Header() {
       <header className="text-gray-400 bg-gray-900 body-font">
         <div className="container mx-auto flex p-5 flex-col md:flex-row items-center justify-between">
           <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-            </svg>
-            <span className="ml-3 text-xl">Tailblocks</span>
+            <img
+              src="/todo_list-icon.png"
+              alt="logo"
+              width="100"
+              height="100"
+            />
           </a>
           <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
             Completed
