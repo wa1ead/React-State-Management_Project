cfe75d5d5024d690714d02c0f59fb855a6225fc1
wa1ead <waliddraa2@gmail.com>
2024-11-27 10:38:38 +0100

    creating the modal component to add new task

---
+++ src/components/InputModal.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+export default function InputModal() {
+  return (
+    <>
+      <div className="justify-center items-center flex overflow-x-hidden overflow-y-hidden fixed inset-0 z-50 outline-none focus:outline-none">
+        <div className="relative container m-8 sm:mx-20 lg:mx-40">
+          {/*content*/}
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-gray-950 outline-none focus:outline-none py-4">
+            {/*header*/}
+            <div className="flex items-center justify-between pb-4 px-4 border-b border-solid border-blueGray-200 rounded-t">
+              <h3 className="text-2xl font-semibold text-gray-300 text-center">
+                Add new Task
+              </h3>
+              <button>
+                <span className="bg-transparent text-white opacity-1 h-6 w-6 text-2xl outline-none focus:outline-none">
+                  ×
+                </span>
+              </button>
+            </div>
+            {/*body*/}
+            <div className="my-8 ">
+              <form
+                action="POST"
+                className="flex flex-col items-center justify-center mx-20"
+              >
+                <input
+                  id="title"
+                  type="text"
+                  required
+                  placeholder="Task Title"
+                  className="w-full mb-8 py-4 px-2 bg-transparent border-2 border-gray-400 rounded-md focus:outline-none focus:ring-0 focus:border-t-transparent focus:border-r-transparent focus:border-l-transparent focus:rounded-none"
+                />
+                <hr />
+                <textarea
+                  id="description"
+                  type="text"
+                  className="w-full mb-4 py-4 px-2 bg-transparent border-2 border-gray-400 text-gray-200 rounded-md focus:outline-none focus:ring-0 focus:border-t-transparent focus:border-r-transparent focus:border-l-transparent focus:rounded-none md:mb-8"
+                  rows="5"
+                >
+                  I wanna achieve my GOALS...!
+                </textarea>
+                <button
+                  type="submit"
+                  className="inline-flex items-center gap-2 text-white bg-green-700 border-0 py-1 px-3 focus:outline-none hover:bg-green-600 rounded text-base mt-2 md:mt-0"
+                >
+                  <i className="fa-solid fa-plus"></i>
+                  <span>Add</span>
+                </button>
+              </form>
+            </div>
+            {/*footer*/}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Welcome from "./Welcome";
 import TaskList from "./TaskList";
+import InputModal from "./InputModal";
 
 export default function Main() {
   const task = true;
-  return task ? <TaskList /> : <Welcome />;
+  // return task ? <TaskList /> : <Welcome />;
+  return <InputModal />;
 }
