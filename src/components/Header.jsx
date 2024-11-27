59c455256ce8216cd783b5e713d2291041a3832a
wa1ead <waliddraa2@gmail.com>
2024-11-27 09:49:42 +0100

    styling add new task button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,19 +13,9 @@ export default function Header() {
               height="100"
             />
           </a>
-          <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
-            Completed
-            <svg
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              className="w-4 h-4 ml-1"
-              viewBox="0 0 24 24"
-            >
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
+          <button className="inline-flex items-center gap-2 text-white bg-green-700 border-0 py-1 px-3 focus:outline-none hover:bg-green-600 rounded text-base mt-4 md:mt-0">
+            <i className="fa-solid fa-plus"></i>Add new Task
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
           </button>
         </div>
       </header>
