5e4d6081de1c6e88f2d238027abc20971c31fd75
wa1ead <waliddraa2@gmail.com>
2024-11-28 11:45:51 +0100

    displaying the modal on edit task click

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,24 +1,42 @@
-import React from "react";
+import { useState } from "react";
+import EditModal from "./EditModal";
 
-export default function TaskItem({ task, key }) {
+export default function TaskItem({ task }) {
+  //THE MODAL STATE
+  const [modal, setModal] = useState(false);
+
+  //HANDLING THE OPEN/CLOSE MODAL BUTTON CLICK
+  const handleClickModal = () => {
+    setModal(!modal);
+    console.log(task);
+  };
+
   return (
-    <div
-      className="flex flex-col items-center border border-gray-400 bg-gray-900 p-4 gap-2 rounded-lg "
-      key={key}
-    >
-      <h2 className="font-semibold text-gray-200">{task.title}</h2>
-      <p className="font-light text-gray-400">{task.description}</p>
-      <div className="w-full flex justify-center items-center gap-6 mt-4 sm:gap-2">
-        <button className="border rounded-full py-1 px-2 bg-yellow-600 transition duration-300 ease-in-out hover:scale-110 hover:bg-yellow-500">
-          <i class="fa-regular fa-pen-to-square"></i>
-        </button>
-        <button className="border rounded-full py-1 px-2 bg-green-600 transition duration-300 ease-in-out hover:scale-110 hover:bg-green-500">
-          Completed <i class="fa-regular fa-square-check"></i>
-        </button>
-        <button className="border rounded-full py-1 px-2 bg-red-600 transition duration-300 ease-in-out hover:scale-110 hover:bg-red-500">
-          <i class="fa-regular fa-trash-can"></i>
-        </button>
-      </div>
-    </div>
+    <>
+      <button onClick={handleClickModal}>
+        <div
+          className="flex flex-col items-center border border-gray-400 bg-gray-900 p-4 gap-2 rounded-lg "
+          key={task.id}
+        >
+          <h2 className="font-semibold text-gray-200">{task.title}</h2>
+          <p className="font-light text-gray-400">{task.description}</p>
+          <div className="w-full flex justify-center items-center gap-6 mt-4 sm:gap-2">
+            <button
+              className="border rounded-full py-1 px-2 bg-yellow-600 transition duration-300 ease-in-out hover:scale-110 hover:bg-yellow-500"
+              onClick={handleClickModal}
+            >
+              <i class="fa-regular fa-pen-to-square"></i>
+            </button>
+            <button className="border rounded-full py-1 px-2 bg-green-600 transition duration-300 ease-in-out hover:scale-110 hover:bg-green-500">
+              Completed <i class="fa-regular fa-square-check"></i>
+            </button>
+            <button className="border rounded-full py-1 px-2 bg-red-600 transition duration-300 ease-in-out hover:scale-110 hover:bg-red-500">
+              <i class="fa-regular fa-trash-can"></i>
+            </button>
+          </div>
+        </div>
+      </button>
+      {modal && <EditModal task={task} onClose={handleClickModal} />}
+    </>
   );
 }
