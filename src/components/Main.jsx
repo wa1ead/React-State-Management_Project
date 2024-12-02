860b69d08cab3cf928c19d06bd63c9da60817f18
wa1ead <waliddraa2@gmail.com>
2024-12-02 15:23:18 +0100

    stylings

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,22 +5,22 @@ import InputModal from "./InputModal";
 import getTasks from "../services/getTasks";
 
 export default function Main() {
-  
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    async function fetchAllTasks() {
-      try {
-        const tasks = await getTasks();
-        setTasks(tasks);
-      } catch (err) {
-        throw err;
-      }
-    }
     fetchAllTasks();
   }, []);
 
-  console.log(tasks);
+  async function fetchAllTasks() {
+    try {
+      const tasks = await getTasks();
+      setTasks(tasks);
+    } catch (err) {
+      throw err;
+    }
+  }
+  // console.log(tasks);
+
   return tasks.length > 0 ? <TaskList tasks={tasks} /> : <Welcome />;
   // return <InputModal />;
 }
