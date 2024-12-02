148942fdc68e440506377520a8a19c03924bf3f9
wa1ead <waliddraa2@gmail.com>
2024-12-02 15:23:07 +0100

    styling the completed tasks section

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -31,33 +31,43 @@ export default function TaskItem({ task }) {
   return (
     <>
       <div
-        className="flex flex-col items-center border border-gray-400 bg-gray-900 p-4 gap-2 rounded-lg "
+        className={`relative flex flex-col items-center border border-gray-400 bg-gray-900 p-4 gap-2 rounded-lg ${
+          task.completion === false ? "h-48" : "h-28"
+        }`}
         key={task.id}
       >
         <button onClick={handleClickModal} className="w-full">
           <h2 className="font-semibold text-gray-200">{task.title}</h2>
-          <p className="font-light text-gray-400">{task.description}</p>
+          <p
+            className={`font-light text-gray-400 ${
+              task.completion === false ? "h-24" : ""
+            } overflow-clip`}
+          >
+            {task.description}
+          </p>
         </button>
-        <div className="w-full flex justify-center items-center gap-6 mt-4 sm:gap-2">
-          <button
-            className="border rounded-full py-1 px-2 bg-yellow-600 transition duration-300 ease-in-out hover:scale-110 hover:bg-yellow-500"
-            onClick={handleClickModal}
-          >
-            <i class="fa-regular fa-pen-to-square"></i>
-          </button>
-          <button
-            className="border rounded-full py-1 px-2 bg-green-600 transition duration-300 ease-in-out hover:scale-110 hover:bg-green-500"
-            onClick={handleCompleted}
-          >
-            Completed <i class="fa-regular fa-square-check"></i>
-          </button>
-          <button
-            className="border rounded-full py-1 px-2 bg-red-600 transition duration-300 ease-in-out hover:scale-110 hover:bg-red-500"
-            onClick={handleClickDelete}
-          >
-            <i class="fa-regular fa-trash-can"></i>
-          </button>
-        </div>
+        {task.completion === false ? (
+          <div className="w-full flex justify-center items-center absolute bottom-2 gap-6 mt-4 sm:gap-2 ">
+            <button
+              className="border rounded-full py-1 px-2 bg-yellow-600 transition duration-300 ease-in-out hover:scale-110 hover:bg-yellow-500"
+              onClick={handleClickModal}
+            >
+              <i class="fa-regular fa-pen-to-square"></i>
+            </button>
+            <button
+              className="border rounded-full py-1 px-2 bg-green-600 transition duration-300 ease-in-out hover:scale-110 hover:bg-green-500"
+              onClick={handleCompleted}
+            >
+              Completed <i class="fa-regular fa-square-check"></i>
+            </button>
+            <button
+              className="border rounded-full py-1 px-2 bg-red-600 transition duration-300 ease-in-out hover:scale-110 hover:bg-red-500"
+              onClick={handleClickDelete}
+            >
+              <i class="fa-regular fa-trash-can"></i>
+            </button>
+          </div>
+        ) : null}
       </div>
       {modal && <EditModal task={task} onClose={handleClickModal} />}
       {deleteModal && <DeleteModal task={task} onClose={handleClickDelete} />}
