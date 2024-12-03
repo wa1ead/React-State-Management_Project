979b088694b7e5ecb3c6c62150485dadc94a9006
wa1ead <waliddraa2@gmail.com>
2024-12-03 13:02:25 +0100

    import the toast provider component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
+      <Toaster />
       <Header />
       <Main />
       <Footer />
